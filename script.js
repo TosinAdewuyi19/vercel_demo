c8ef4ad1729a213b8f3d12bd0ad5ec70bdf2899f
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2025-04-17 10:09:02 +0100

    demo

---
+++ script.js
@@ -0,0 +1,15 @@
+const wrapper = document.getElementById("wrapper");
+
+console.log(wrapper);
+
+const pageBanner = document.getElementById('page-banner')
+console.log(pageBanner)
+
+const title = document.getElementsByClassName('title')
+
+const titles = Array.from(title)
+
+titles.forEach((title)=>{
+    let parent = 
+    console.log(title.textContent)
+})
